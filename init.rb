4dd4074120177e368efa95ff2c9aa303cbe085ee
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2010-07-02 00:36:12 +0200

    make load process bundler (or any package management tool) firendly

--- init.rb
+++ init.rb
@@ -1,1 +1,6 @@
-require "rails/init"
+require 'action_controller/base'
+
+# Allow whatever Ruby Package tool is being used to manage load paths. Also prevents from loading subdomain fu twice.
+require 'subdomain-fu' unless defined?(SubdomainFu)
+
+ActionController::Base.send :include, SubdomainFu::Controller
