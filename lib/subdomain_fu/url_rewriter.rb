cc3f229174c98ed7642a2c6743eb1ac990405470
Pat Allan <pat@freelancing-gods.com>
2010-09-21 18:25:52 +1000

    Allowing for calling routes without a request, and just generally making this method a little more readable.

--- lib/subdomain_fu/url_rewriter.rb
+++ lib/subdomain_fu/url_rewriter.rb
@@ -4,14 +4,18 @@ module ActionDispatch
   module Routing
     class RouteSet #:nodoc:
       def url_for_with_subdomains(options, path_segments=nil)
-        if SubdomainFu.needs_rewrite?(options[:subdomain], (options[:host] || @request.host_with_port)) || options[:only_path] == false
+        host_with_port   = options[:host]
+        host_with_port ||= @request.host_with_port unless @request.nil?
+        subdomain        = options.delete(:subdomain)
+        
+        if options[:only_path] == false || SubdomainFu.needs_rewrite?(subdomain, host_with_port)
           options[:only_path] = false if SubdomainFu.override_only_path?
-          options[:host] = SubdomainFu.rewrite_host_for_subdomains(options.delete(:subdomain), options[:host] || @request.host_with_port)
-        else
-          options.delete(:subdomain)
+          options[:host] = SubdomainFu.rewrite_host_for_subdomains(subdomain, host_with_port)
         end
+        
         url_for_without_subdomains(options)
       end
+      
       alias_method_chain :url_for, :subdomains
     end
   end
